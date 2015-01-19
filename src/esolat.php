e0b99cc52843b9db1508317229b56e096ca50d4f
natsu90 <solomon_scorpio90@yahoo.com>
2015-01-19 22:06:44 +0800

    update to goutte

--- src/esolat.php
+++ src/esolat.php
@@ -3,6 +3,13 @@ use Goutte\Client;
 
 class Esolat {
 	
+	public function getWaktuSolat($zoneCode, $timeString = 'today')
+	{
+		$client = new GuzzleHttp\Client();
+
+		
+	}
+
 	public function test()
 	{
 		return 'foo';
